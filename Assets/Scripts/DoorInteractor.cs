d2be5d3901f0147ae55bd7b565e02db23676c2b5
mbattaloglu <mmbattaloglu@gmail.com>
2023-02-16 00:25:59 +0300

    Door and Window Intertactions

---
+++ Assets/Scripts/DoorInteractor.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteractor : MonoBehaviour
+{
+    public Animator animator;
+    public Transform player;
+    private bool isOpen;
+
+    private void Start()
+    {
+        isOpen = false;
+    }
+
+    public void Interact()
+    {
+        if (player)
+        {
+            float dist = Vector3.Distance(player.position, transform.position);
+            if (dist < 15)
+            {
+                Debug.Log("Distance is " + dist);
+                Debug.Log("Player position: " + player.position);
+                Debug.Log("Door position: " + transform.position);
+                if (!isOpen)
+                {
+                    StartCoroutine(Open());
+                }
+                else
+                {
+                    StartCoroutine(Close());
+                }
+            }
+        }
+    }
+
+    IEnumerator Open()
+    {
+        animator.Play("Opening");
+        isOpen = true;
+        yield return new WaitForSeconds(.5f);
+    }
+    
+    IEnumerator Close()
+    {
+        animator.Play("Closing");
+        isOpen = false;
+        yield return new WaitForSeconds(.5f);
+    }
+}
